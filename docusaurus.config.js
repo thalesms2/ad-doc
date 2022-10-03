a14fca0739a8daba5a3185f7679f461413ae1ccb
thales sato <thalesms2@outlook.com>
2022-10-03 12:14:14 -0300

    feat: search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -77,6 +77,29 @@ const config = {
           },
         ],
       },
+      algolia: {
+      // The application ID provided by Algolia
+      appId: 'EZPVM4M1G9',
+
+      // Public API key: it is safe to commit it
+      apiKey: '8b992f699c4b4b5a7d2a771b6ef7b048',
+
+      indexName: 'ad_DOC',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+
+      //... other Algolia params
+      },
       footer: {
         style: 'dark',
         links: [
